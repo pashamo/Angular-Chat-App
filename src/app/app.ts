7c71d588bb6d9cbdbe0e1a0a6ec742db395bb9e7
Mohammed Pasha <mk.pasha@hotmail.com>
2026-05-18 00:03:49 -0400

    add: login and chat components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './pages/login-component/login-component';
+import { ChatComponent } from './pages/chat-component/chat-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, ChatComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
