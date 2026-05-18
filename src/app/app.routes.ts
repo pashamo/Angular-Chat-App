b2421f292a4301e6622923e10c6787dbe7c98927
Mohammed Pasha <mk.pasha@hotmail.com>
2026-05-18 15:38:14 -0400

    add: environments and auth services placeholders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+// import { ChatComponent } from './pages/chat-component/chat-component';
+// import { LoginComponent } from './pages/login-component/login-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'chat',
+        // component: ChatComponent
+        loadComponent: () => import('./pages/chat-component/chat-component').then((com)=>com.ChatComponent),
+    },
+    {
+        path: 'login',
+        // component: LoginComponent
+        loadComponent: () => import('./pages/login-component/login-component').then((com)=>com.LoginComponent),
+    },
+    {
+        path: '',
+        // component: LoginComponent
+        loadComponent: () => import('./pages/login-component/login-component').then((com)=>com.LoginComponent),
+    }
+];
